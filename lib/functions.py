a51102cc8df3315a742fd9ecbeac0be218e2f167
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-11-21 11:20:50 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def greet(name):
+def greet(name="Deno"):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum_result= add(45, 55)
+print(sum_result)
+pass
 
-def halve(number):
-    pass
+def halve(number):   
+    return number/2
+
+pass
